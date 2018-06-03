cd5ff5da9c923a0a8e1c21067018eeb0d61992c5
Peter Gwaka <pgwaka@gmail.com>
2018-06-03 19:02:24 +0200

    added buton and route handlers

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        {/*<button type="button" onClick={() => signIn()}>Sign In Using Google</button>*/}
+        <a href="/auth/google">Sign In Using Google</a>
       </div>
     );
   }
--- services/passport.js
+++ services/passport.js
@@ -22,19 +22,14 @@ passport.use(
       clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback',
       proxy: true
-    }, (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id })
-        .then((existingUser) => {
-        if (existingUser) {
-          // we already have a record
-          console.log('user exists');
-          done(null, existingUser);
-        } else {
-          // create user
-          new User({ googleId: profile.id, username: profile.displayName })
-              .save()
-            .then( user => done(null, user));
-        }
-      });
+    }, async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+
+      const user = await new User({ googleId: profile.id, username: profile.displayName }).save();
+      done(null, user);
     })
 );
